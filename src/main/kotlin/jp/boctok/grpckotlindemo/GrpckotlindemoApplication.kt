e13a3361417206e0d1ad1d637d383609ee3a01f9
M.Higashijima <boctok.ctpoba@gmail.com>
2020-10-29 23:24:55 +0900

    made by spring initializr

---
+++ src/main/kotlin/jp/boctok/grpckotlindemo/GrpckotlindemoApplication.kt
@@ -0,0 +1,11 @@
+package jp.boctok.grpckotlindemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrpckotlindemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<GrpckotlindemoApplication>(*args)
+}
---
+++ src/test/kotlin/jp/boctok/grpckotlindemo/GrpckotlindemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package jp.boctok.grpckotlindemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GrpckotlindemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
